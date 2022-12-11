314e792cdae0a306f6b8305ddafa51487f323b02
nayem9b <razwanniam1@gmail.com>
2022-12-11 21:06:38 +0600

    link fixed

--- src/Components/ProjectDetails/ProjectDetails.js
+++ src/Components/ProjectDetails/ProjectDetails.js
@@ -16,27 +16,27 @@ const ProjectDetails = () => {
         <img src={img3} className=' ' alt='' />
       </div>
       <div className='text-center mx-auto mt-4'>
-        <Link to={client}>
+        <a href={client} target='_blank'>
           <button
             type='button'
             class='inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out '>
             Client side
           </button>
-        </Link>
-        <Link to={server}>
+        </a>
+        <a href={server} target='_blank'>
           <button
             type='button'
             class='inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out ml-4'>
             Server Side
           </button>
-        </Link>
-        <Link to={website}>
+        </a>
+        <a href={website} target='_blank'>
           <button
             type='button'
             class='inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out ml-4'>
             Live website
           </button>
-        </Link>
+        </a>
       </div>
       <h1>Details:</h1>
 
