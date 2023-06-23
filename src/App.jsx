ee69ab3eb814502617d88bd52f3eadd04fe6ff0e
nayem9b <razwanniam1@gmail.com>
2023-06-23 18:07:30 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,36 @@
 /* eslint-disable no-unused-vars */
 
-import { BrowserRouter } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import About from "./components/About"
-import Experience from "./components/Experience"
-import Tech from "./components/Tech"
-import Works from "./components/Works"
-import Feedbacks from "./components/Feedbacks"
-import Contact from "./components/Contact"
-import Stars from "./components/canvas/Stars"
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+import Feedbacks from "./components/Feedbacks";
+import Contact from "./components/Contact";
+import Stars from "./components/canvas/Stars";
 
 function App() {
-
-
   return (
- <BrowserRouter>
- <div className="relative z-0 bg-primary" >
-<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-<Navbar></Navbar>
-<Hero></Hero>
-
-
-</div>
-<About></About>
-<Experience></Experience>
-  <Tech></Tech>
-  <Works></Works>
-  <Feedbacks></Feedbacks>
-  <div className="relative z-0">
-<Contact></Contact>
-<Stars></Stars>
-
-  </div>
- </div>
- 
- </BrowserRouter>
-  )
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar></Navbar>
+          <Hero></Hero>
+        </div>
+        <About></About>
+        <Experience></Experience>
+        <Tech></Tech>
+        <Works></Works>
+        <Feedbacks></Feedbacks>
+        <div className="relative z-0">
+          <Contact></Contact>
+          <Stars></Stars>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
