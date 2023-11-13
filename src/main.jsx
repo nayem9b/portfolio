c635ce99f918b77a6878faa4fd390b4ff8258350
Md Razwan Niam <razwanniam1@gmail.com>
2023-11-13 15:27:19 +0600

    messenger chatbot added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <MessengerCustomerChat pageId="183080354880130" appId="168501859620532" />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
